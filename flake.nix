0de462647fc36b83638991704525bd5d6cf26eba
Piotr Bogdan <ppbogdan@gmail.com>
2020-09-29 19:34:30 +0100

    nix: add flake support

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very simple standalone tray";
+
+  inputs = {
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    libcarbontray = {
+      url = "github:pbogdan/libcarbontray";
+    };
+
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, flake-utils, libcarbontray, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          carbontray = pkgs.callPackage ./carbontray.nix {
+            libcarbontray = libcarbontray.packages.${system}.libcarbontray;
+          };
+        };
+
+        defaultPackage = self.packages.${system}.carbontray;
+      }
+    );
+}
